a59a1bcb8d7d0b9ec52c140fee01837afead1fe2
Ahmad Fathi <ahmed.fathi.aziz@gmail.com>
2025-05-30 15:51:51 +0300

    edit .gitignore

--- prisma/migrations/20250528014741_add_gallery_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Gallery" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "imageUrl" TEXT NOT NULL,
-    "caption" TEXT NOT NULL,
-    "captionAr" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
